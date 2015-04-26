0a87b59fc2fb4885578109ef3f848e3bb7a4dbdd
Mohd Zeeshan Shaikh <mohdzeeshan.shaikh@sjsu.edu>
2015-04-25 23:45:01 -0700

    Initial UI

---
+++ ShareIt/ShareIt/SIDashboardViewController.h
@@ -0,0 +1,15 @@
+//
+//  SIDashboardViewController.h
+//  ShareIt
+//
+//  Created by student on 4/25/15.
+//  Copyright (c) 2015 Example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SIDashboardViewController : UIViewController <UIScrollViewDelegate>
+
+@property (nonatomic, strong) UILabel *labelTest;
+
+@end
---
+++ ShareIt/ShareIt/SIDashboardViewController.m
@@ -0,0 +1,13 @@
+//
+//  SIDashboardViewController.m
+//  ShareIt
+//
+//  Created by student on 4/25/15.
+//  Copyright (c) 2015 Example. All rights reserved.
+//
+
+#import "SIDashboardViewController.h"
+
+@implementation SIDashboardViewController
+
+@end
